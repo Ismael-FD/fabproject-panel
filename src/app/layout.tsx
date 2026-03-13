5cd5531d886f889ca876cd5a2ce85b88d02770a1
Ismael-FD <ismacapo2016@hotmail.com>
2026-03-13 03:07:54 -0300

    cambios pequeño

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata = {
-  title: "FaChat - Panel",
+  title: "Panel - FaChat",
   description: "Panel de administración para FaChat",
 };
 
